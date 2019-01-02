f4f41d2551adb26a13a0aee7ab4ec278fa1cd55a
zhangshuai <zhangshuai@fence-labs.com>
2019-01-02 12:08:42 +0800

    on dev add a method

--- app/src/main/java/com/example/zhangshuai/gitlearingdemo/CalUtils.kt
+++ app/src/main/java/com/example/zhangshuai/gitlearingdemo/CalUtils.kt
@@ -1,4 +1,23 @@
 package com.example.zhangshuai.gitlearingdemo
 
-class CalUtils {
+object CalUtils {
+    /**
+     * 遍历集合
+     */
+    fun traversingList(list: MutableList<String>) {
+        for (str in list) {
+            println(str)
+        }
+    }
+
+    /**
+     * @strs
+     * 生成集合
+     */
+    fun createList(vararg strs: String) {
+        val list = ArrayList<String>()
+        for (str in strs) {
+            list.add(str)
+        }
+    }
 }
