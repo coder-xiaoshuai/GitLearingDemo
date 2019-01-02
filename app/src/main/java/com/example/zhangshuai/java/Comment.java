476b8f1ee6dd3487e80fec823f3e68911202e27d
zhangshuai <zhangshuai@fence-labs.com>
2019-01-02 15:21:55 +0800

    增加了一个注释类

---
+++ app/src/main/java/com/example/zhangshuai/java/Comment.java
@@ -0,0 +1,17 @@
+package com.example.zhangshuai.java;
+
+public class Comment {
+    /*
+     *
+     *
+     *       这个类只有一个注释
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     * */
+}
