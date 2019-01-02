656c87d3401bf3f790db9cacd94511211a2e4e70
zhangshuai <zhangshuai@fence-labs.com>
2019-01-02 12:17:59 +0800

    增加了一个person类

--- app/src/main/java/com/example/zhangshuai/java/Person.java
+++ app/src/main/java/com/example/zhangshuai/java/Person.java
@@ -1,4 +1,31 @@
 package com.example.zhangshuai.java;
 
 public class Person {
+    private String name;
+    private int age;
+    private int weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
