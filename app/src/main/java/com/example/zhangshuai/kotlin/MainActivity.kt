34f74600b23490bd88254a9964a1245a6e748091
zhangshuai <zhangshuai@fence-labs.com>
2019-01-02 15:49:22 +0800

    增加了点击事件

--- app/src/main/java/com/example/zhangshuai/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/zhangshuai/kotlin/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.zhangshuai.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.zhangshuai.gitlearingdemo.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text.setOnClickListener {
+            Toast.makeText(this@MainActivity, "提示信息", Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun getCalResult(number1: Int, number2: Int): Int {
