1f6c6b8637a1a8878302b941e1f2eb5e27cc4aa5
zhangshuai <zhangshuai@fence-labs.com>
2019-01-02 18:13:20 +0800

    增加了temp4

--- app/src/main/java/com/example/zhangshuai/java/Dev.java
+++ app/src/main/java/com/example/zhangshuai/java/Dev.java
@@ -1,9 +1,11 @@
 package com.example.zhangshuai.java;
 
 public class Dev {
-    public static void temp1(){}
+    public static void temp1() {}
 
-    public static void temp2(){}
+    public static void temp2() {}
 
-    public static void temp3(){}
+    public static void temp3() {}
+
+    public static void tomp4() {}
 }
