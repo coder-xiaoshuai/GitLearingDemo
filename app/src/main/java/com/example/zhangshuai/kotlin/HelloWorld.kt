972a1df14eb2a1d3295d32f3bbc448fa785f7241
zhangshuai <zhangshuai@fence-labs.com>
2019-04-20 14:40:19 +0800

    学习kotlin中 when的用法

--- app/src/main/java/com/example/zhangshuai/kotlin/HelloWorld.kt
+++ app/src/main/java/com/example/zhangshuai/kotlin/HelloWorld.kt
@@ -14,7 +14,7 @@ class HelloWorld {
     }
 
 
-    fun getShape(shape:SHAPE):String {
+    fun getShape(shape: SHAPE): String {
         return when (shape) {
             SHAPE.TRIANGLE -> {
                 "三角形"
@@ -34,11 +34,55 @@ class HelloWorld {
     class Person(name: String, age: Int, sex: Boolean)
 
     enum class SHAPE(perimeter: Int) {
-        TRIANGLE(100), RECT(120), CIRCLE(130),NONE(-1)
+        TRIANGLE(100), RECT(120), CIRCLE(130), NONE(-1)
     }
 
-    class Circle{
+    class Circle {
         var radius = 0
+    }
+
+    /**
+     * 枚举测试
+     */
+    enum class Color(val r: Int = 0, val g: Int = 0, val b: Int = 0) {
+        RED(255, 0, 0), ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLEF
+    }
+
+    /**
+     * 根据分数获取成绩等级
+     */
+    fun getScoreLevel(score: Int): String {
+        when (score) {
+            in 90..100 -> {
+                return "A"
+            }
+            in 80..90 -> {
+                return "B"
+            }
+            in 70..80 -> {
+                return "C"
+            }
+            in 60..70 -> {
+                return "D"
+            }
+            else -> {
+                return "不及格"
+            }
+        }
+    }
+
+    /**
+     * when不带参数用法
+     */
+    fun getSumBigerThanTarget(number1: Int, number2: Int, target: Int): Boolean {
+        when {
+            (number1 + number2 > target) -> {
+                return true;
+            }
+            else -> {
+                return false;
+            }
+        }
     }
 
 }
