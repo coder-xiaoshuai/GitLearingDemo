9a2fd4626fd26d25b3fec70e5dfd14558e770030
xiaoshuai <540958710@qq.com>
2020-06-02 22:55:15 +0800

    算法练习

---
+++ lib_java_test/src/main/java/com/example/lib_java_test/AlgorithmTest.java
@@ -0,0 +1,31 @@
+package com.example.lib_java_test;
+
+public class AlgorithmTest {
+
+    public static void main(String[] args) {
+        System.out.println(cutRope(4));
+    }
+
+    /**
+     * 算法题：剪绳子
+     * 给你一根长度为n的绳子，请把绳子剪成整数长的m段（m、n都是整数，n>1并且m>1），
+     * 每段绳子的长度记为k[0],k[1],...,k[m]。请问k[0]xk[1]x...xk[m]可能的最大乘积是多少？
+     * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+     *
+     * 主要考动态规划
+     */
+    public static int cutRope(int n) {
+        if (n == 2) {
+            return 1;
+        }
+        if (n == 3) {
+            return 2;
+        }
+        int sum = 1;
+        while (n > 4) {
+            n = n - 3;
+            sum = sum * 3;
+        }
+        return sum*n;
+    }
+}
