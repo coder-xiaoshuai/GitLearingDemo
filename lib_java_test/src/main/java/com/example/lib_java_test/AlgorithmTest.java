0a77da5118f288d3f3955b5b03624d6981c9a730
xiaoshuai <540958710@qq.com>
2020-06-15 23:14:13 +0800

    两数之和

--- lib_java_test/src/main/java/com/example/lib_java_test/AlgorithmTest.java
+++ lib_java_test/src/main/java/com/example/lib_java_test/AlgorithmTest.java
@@ -279,21 +279,21 @@ public class AlgorithmTest {
      * @return
      */
     public static ArrayList<Integer> maxInWindows(int[] num, int size) {
-            ArrayList<Integer> result = new ArrayList<>();
-            if (num == null || size == 0 || size > num.length) {
-                return result;
-            }
-            int resultSize = num.length - size + 1;
-            for (int i = 0; i < resultSize; i++) {
-                int max = num[i];
-                for (int j = i; j < i + size; j++) {
-                    if (num[j] > max) {
-                        max = num[j];
-                    }
-                }
-                result.add(max);
-            }
+        ArrayList<Integer> result = new ArrayList<>();
+        if (num == null || size == 0 || size > num.length) {
             return result;
+        }
+        int resultSize = num.length - size + 1;
+        for (int i = 0; i < resultSize; i++) {
+            int max = num[i];
+            for (int j = i; j < i + size; j++) {
+                if (num[j] > max) {
+                    max = num[j];
+                }
+            }
+            result.add(max);
+        }
+        return result;
     }
 
     public int[] maxSlidingWindow(int[] nums, int size) {
@@ -313,5 +313,36 @@ public class AlgorithmTest {
             result[i] = max;
         }
         return result;
+    }
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，
+     * 并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            int temp = target - nums[i];
+            for (int j = i + 1; j < length; j++) {
+                if (temp == nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+
     }
 }
