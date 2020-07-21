8a5a768985d3da4b6fafe2e9ea70a2a7612f8abc
zhangshuai <zhangshuai1@miliantech.com>
2020-07-21 21:11:31 +0800

    生成tree

--- lib_java_test/src/main/java/com/example/lib_java_test/AlgorithmTest.java
+++ lib_java_test/src/main/java/com/example/lib_java_test/AlgorithmTest.java
@@ -2148,4 +2148,41 @@ public class AlgorithmTest {
         return result;
     }
 
+    /**
+     * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+     * @param n
+     * @return
+     */
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+        //枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            //获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            //获得所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+            //从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (TreeNode leftTree : leftTrees) {
+                for (TreeNode rightTree : rightTrees) {
+                    TreeNode currentNode = new TreeNode(i);
+                    currentNode.left = leftTree;
+                    currentNode.right = rightTree;
+                    allTrees.add(currentNode);
+                }
+            }
+        }
+        return allTrees;
+    }
+
 }
